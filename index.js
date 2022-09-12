39a92fc9ae8f4e75eebac143c690312d0fb64e70
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-09-12 14:55:16 +0530

    Solve the CORS error.

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 var db = require('./database');
 'use strict';
 
-// parse request body as JSON
+// Solve the CORS error from here
+app.use(cors({
+    origin: '*'
+}));
+
+// parse request body as JSON, Always use on start of the file like that.
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
+// API to register student (http://localhost:8080/registeruser) POST.
 app.post("/registeruser", (req, res) => {
     let name = req.body.name;
     let email = req.body.email;
     let mobile = req.body.mobile;
     let cource = req.body.cource;
 
+    // here register_student is db name and it has to be same in db also.
     var sql = `INSERT INTO register_student (name, email, mobile, cource) VALUES ("${name}", "${email}", "${mobile}", "${cource}")`;
 
-    
     db.query(sql, function (err, result) {
         if (err) throw err;
         console.log('record inserted');
